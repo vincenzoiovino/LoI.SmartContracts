d22e21e1588b1cfbb5df17054234911918242fa3
vincenzoiovino <vinciovino@gmail.com>
2024-08-23 12:34:23 +0200

    ssl

--- web/server.js
+++ web/server.js
@@ -1,13 +1,38 @@
-const PORT = 5001;
+var PORT = 5001;
+const commander = require('commander');
 const express = require('express');
-const app = express();
 const nocache = require('nocache');
+const http = require('http');
+const https = require('https');
 const cors = require('cors');
+const fs = require('fs');
+commander
+    .version('1.0.0', '-v, --version')
+    .usage('-p <value> -s <value>')
+    .option('-p, --port <value>', 'port on which to listen. Default to 5001')
+    .option('-ssl, --ssl', 'use ssl.')
+    .option('-c, --cer <value>', 'certificate of the server.')
+    .option('-k, --key <value>', 'secret key of the server.')
+    .parse(process.argv);
+const options = commander.opts();
+
+
+var httpsServer;
+
+if (options.ssl) {
+const privateKey  = fs.readFileSync(options.cer, 'utf8');
+const certificate = fs.readFileSync(options.key, 'utf8');
+
+const credentials = {key: privateKey, cert: certificate};
+httpserver = https.createServer(credentials, app);
+
+
+}
+const app = express();
 app.use(nocache());
 app.use(cors());
-
-
-// start the express web server listening on 81
+if (options.port) PORT= options.port;
+// start the express web server listening on PORT
 app.listen(PORT, () => {
     console.log('listening on ' + PORT);
 });
